505bdffb04285cd88bc42f9cdbba3b795f3e29ad
Shirley <charquecaballeroshirley@gmail.com>
2025-10-29 23:58:09 -0400

    Agregar permisos de Filament al modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -41,4 +43,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Determinar si el usuario puede acceder al panel de Filament
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Permitir acceso a todos los usuarios autenticados
+        return true;
+    }
 }
